967b3f153bd9b608439cc1192c4fb49859d923e5
Alex Koo <akoo@apigee.com>
2015-11-16 13:12:09 -0800

    pass local variable in shell command

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
 var shelljs = require('shelljs');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 
 
 app.post('/deploy', function (req, res) {
-    // res.send('Hello World!');
+    var repo = req.body.repo;
+    var apiFolder = req.body.apiFolder;
+    var makeScript = req.body.makeScript;
+    var org = req.body.org;
+    var env = req.body.env;
+    var userName = req.body.userName;
+    var pw = req.body.pw;
 
-    var repo = req.query.repo;
-    var apiFolder = req.query.apiFolder;
-    var makeScript = req.query.makeScript;
-    var org = req.query.org;
-    var env = req.query.env;
-
+    console.log(userName);
     console.log(repo);
     console.log(apiFolder);
     console.log(makeScript);
     console.log(org);
     console.log(env);
 
-    shelljs.exec("git clone "+repo+" "+org+env, function(code, output){
-            console.log('Exit code:', code);
-            console.log('Program output:', output);
-            shelljs.cd(org+env);
-            console.log(shelljs.exec(apiFolder+"/"+makeScript));
-    });
+    console.log(shelljs.exec('ae_username="'+userName+'" sh -c \'echo "Hello $ae_username"\''));
+
+//     while(true){
+//     console.log(shelljs.exec("echo $ae_username"));
+// }
+    // shelljs.exec("git clone "+repo+" "+org+env, function(code, output){
+    //         console.log('Exit code:', code);
+    //         console.log('Program output:', output);
+    //         shelljs.cd(org+env+"/"+apiFolder);
+    //         console.log(shelljs.exec(makeScript));
+    // });
 
     // console.log(shelljs.exec('sh testing/apiproxies/apigee-nodejs-fileserver/make.sh'));
 
