774583cb7083e39818a8bbc6a45cb52d584c87f8
dzuluaga <dzuluaga@apigee.com>
2015-12-06 20:56:51 -0800

    ability to upload zip files

--- README.md
+++ README.md
@@ -3,11 +3,18 @@ Deploy Now button for Apigee Edge samples.
 
 [![Deploy to Apigee](./images/deploy_to_apigee.png)](http://dabuttonfactory.com/#t=Deploy+to+Apigee&f=Calibri-Bold-Italic&ts=22&tc=fff&hp=35&vp=10&c=round&bgt=pyramid&bgc=f90&ebgc=f90&shs=4&shc=666&sho=se)
 
-##### Trigger make API
+##### cURL to Github Repo
 ```shell
 curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'repo=https%3A%2F%2Fgithub.com%2Fakoo1010%2Fapigee-tutorials.git' -d 'apiFolder=apiproxies%2Fapigee-nock-mock' -d 'makeScript=make.sh' \
 -d 'org=testmyapi' -d 'env=test' -d 'userName='$ae_username'' -d 'pw='$ae_password'' 'http://localhost:3000/deploy' -v
 ```
+##### cURL with zip file
+```shell
+curl -v -X POST -F "zipFile=@Mavendeploynow-master.zip" -F "makeScript=makeScript.sh" -F "apiFolder=Mavendeploynow-master/src/gateway/forecastweatherapi/" -F "org=testmyapi" -F "env=test" \
+-F userName=$ae_username -F pw=$ae_password 'http://localhost:3000/deploy_zip'
+```
+
+
 
 ##### Access Login App
 http://localhost:3000/login-form/
--- app.js
+++ app.js
@@ -8,12 +8,61 @@ app.use(express.static('public'));
 //var rimraf = bluebird.promisifyAll(require('rimraf'));
 var rimraf = require('rimraf');
 var path = require('path');
-var util = require('./lib/util');
+var _util = require('./lib/util');
+var multer = require('multer');
+var upload = multer({ dest: __dirname + '/uploads/' });
+var fs = require('fs');
+var unzip = require('unzip');
 var config = require('./config');
 
 app.use(cors());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.post('/deploy_zip', upload.single('zipFile'), function (req, res, next) {
+  shelljs.cd(__dirname);
+  console.log( req.body );
+  var apiFolder = req.body.apiFolder || ".";
+  var makeScript = req.body.makeScript || "make.sh";
+  var org = req.body.org || "";
+  var env = req.body.env || "";
+  if( !org || !env){
+    throw("Missing org or env parameters.");
+  }
+  res.write('apiFolder: ' + apiFolder + '\n');
+  res.write('makeScript: ' + makeScript + '\n');
+  res.write('org: ' + org + '\n');
+  res.write('env: ' + env + '\n');
+  var userName = req.body.userName;
+  var pw = req.body.pw;
+  var UUID = _util.sessionToken();
+  res.write('UUID: ' + UUID + '\n');
+  var nodeBasePathBuild = path.join('./public/builds/', UUID); //node.js app is based on /public
+  var loginAppBasePathBuild = path.join('/builds/', UUID); //login app is based on /public/.
+  var appBasePath = process.cwd();
+  var pathToDirections = 'directions.html' || req.body.pathToDirections; //directions.html
+  var stream = fs.createReadStream(req.file.path).pipe(unzip.Extract({ path: nodeBasePathBuild }));
+  stream.on('close', function() {
+    shelljs.cd(nodeBasePathBuild + "/" + apiFolder);
+    var execStr = ' ae_username=' + userName + ' ae_password=' + pw + ' env=' + env //space in front of ae_username to run command in bash without save in history
+        + ' org=' + org + ' sh -c \'sh ' + makeScript + '\'';
+    res.write("============= Initiating Build ===============\n");
+    console.log(execStr);
+    var output = shelljs.exec( execStr, { async: true});
+    output.stdout.on('data', function(data) {
+      res.write(data);
+    });
+    output.stdout.on('end', function(code) {
+      res.write('============== Deployment Completed ===============\n');
+      setTimeout( _util.removeClonedRepo, config.remove_cloned_repo_timeout, appBasePath, nodeBasePathBuild);
+      res.end();
+    });
+    output.stderr.on('data', function(code) {
+      res.write(code);
+    });
+
+  });
+});
 
 app.post('/deploy', function (req, res) {
     shelljs.cd(__dirname);
@@ -23,9 +72,9 @@ app.post('/deploy', function (req, res) {
     var org = req.body.org || "";
     var env = req.body.env || "";
     if( !repo ) throw("Missing repo.");
-    // if( !org || !env){
-    //   throw("Missing org or env parameters.");
-    // }
+    if( !org || !env){
+      throw("Missing org or env parameters.");
+    }
     res.write('repo: ' + repo + '\n');
     res.write('apiFolder: ' + apiFolder + '\n');
     res.write('makeScript: ' + makeScript + '\n');
@@ -33,23 +82,13 @@ app.post('/deploy', function (req, res) {
     res.write('env: ' + env + '\n');
     var userName = req.body.userName;
     var pw = req.body.pw;
-    var UUID = util.sessionToken();
+    var UUID = _util.sessionToken();
     res.write('UUID: ' + UUID + '\n');
     var nodeBasePathBuild = path.join('./public/builds/', UUID); //node.js app is based on /public
     var loginAppBasePathBuild = path.join('/builds/', UUID); //login app is based on /public/.
-    var appBasePath = process.cwd()
-    // var nodeBasePathBuild = path.join('./public/builds/', util.sessionToken(), org + '-' + env);
+    var appBasePath = process.cwd();
+    // var nodeBasePathBuild = path.join('./public/builds/', _util.sessionToken(), org + '-' + env);
     var pathToDirections = 'directions.html' || req.body.pathToDirections; //directions.html
-    //** var linktoGithub =  window.referer || req.body.linktoGithub;  test window.referer
-    //console.log(nodeBasePathBuild);
-    //rimraf.sync(nodeBasePathBuild);
-    // console.log(userName);
-    // console.log(repo);
-    // console.log(apiFolder);
-    // console.log(makeScript);
-    // console.log(org);
-    // console.log(env);
-
     shelljs.exec("git clone " + repo + " " + nodeBasePathBuild, function(code, output) {
       console.log("Exit:", code);
       res.write("Exit: " + code + "\n");
@@ -60,36 +99,15 @@ app.post('/deploy', function (req, res) {
       var execStr = ' ae_username=' + userName + ' ae_password=' + pw + ' env=' + env //space in front of ae_username to run command in bash without save in history
         + ' org=' + org + ' sh -c \'sh ' + makeScript + '\'';
       res.write("============= Initiating Build ===============\n");
-      //console.log( execStr );
-      // var output = shelljs.exec( execStr, function(code, output) {
-      //     console.log('Exit code:', code);
-      //     console.log('Program output:', output);
-      //     console.log(output);
-      //     // res.json({code: code, output: output, loginAppBasePathBuild: loginAppBasePathBuild, pathToDirections: pathToDirections});
-      //
-      //     //timeout can be supplied when loading up server, e.g., timeout=10000 node app for ~10s cleanup
-      //     // setTimeout( util.removeClonedRepo, config.remove_cloned_repo_timeout, appBasePath, nodeBasePathBuild);
-      //
-      // } )
-
-      // var output = shelljs.exec( execStr, function(code, output) {
-      //     console.log('Exit code:', code);
-      //     console.log('Program output:', output);
-      //     console.log(output);
-      //     res.json({code: code, output: output, loginAppBasePathBuild: loginAppBasePathBuild});
-
       var output = shelljs.exec( execStr, { async: true});
       output.stdout.on('data', function(data) {
-        output.stdout
         res.write(data);
       });
-
       output.stdout.on('end', function(code) {
         res.write('============== Deployment Completed ===============\n');
-        setTimeout( util.removeClonedRepo, config.remove_cloned_repo_timeout, appBasePath, nodeBasePathBuild);
+        setTimeout( _util.removeClonedRepo, config.remove_cloned_repo_timeout, appBasePath, nodeBasePathBuild);
         res.end();
       });
-
       output.stderr.on('data', function(code) {
         res.write(code);
       });
